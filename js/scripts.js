ff624a0fe4f582dbe962677e2504a2e7753bd0fb
Cesar Vieira <cesardiogovieira2003@gmail.com>
2024-03-29 14:54:16 +0000

    Editing the script

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,12 @@
 console.log("hello");
 
-// Store the menu toggler element in a variable
-const menuToggler = document.getElementById('menuToggler');
 
-// Store the menu element in a variable
-const menu = document.getElementById('menu');
-
-// Function to toggle the menu visibility and update menu toggler text
 menuToggler.addEventListener('click', ev => {
     menu.classList.toggle('open');
     menuToggler.textContent = menuToggler.textContent === "×" ? "≡" : "×";
-    toggleHeaderVisibility(); // Call toggleHeaderVisibility after toggling the menu
 });
 
-// Store the header element in a variable
+
 const header = document.querySelector('header');
 
 // Define the scroll threshold at which the header should disappear
@@ -21,7 +14,7 @@ const scrollThreshold = 100; // Adjust this value as needed
 
 // Function to toggle the header visibility
 function toggleHeaderVisibility() {
-    if (window.scrollY > scrollThreshold && !menuToggler.classList.contains('open')) {
+    if (window.scrollY > scrollThreshold) {
         header.classList.add('hidden');
     } else {
         header.classList.remove('hidden');
@@ -30,6 +23,3 @@ function toggleHeaderVisibility() {
 
 // Add event listener for scroll event
 window.addEventListener('scroll', toggleHeaderVisibility);
-
-// Call toggleHeaderVisibility initially to handle header visibility based on initial scroll position
-toggleHeaderVisibility();
